058aa3a5a7cb41843adb09c63ea082e0c9ede551
Belal Kohgadai <kohgadai@gmail.com>
2015-02-18 21:39:10 -0800

    home controller now discerns the date, retrieves shows, and adds it all to scope.

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('tvfeeder')
-  .controller('HomeCtrl', function($scope) {
-    $scope.yeogurt = 'awesome';
+  .controller('HomeCtrl', function($scope, $routeParams, $filter, thisDaysShows, nextListing, prevListing) {
+    var todaysDate = $routeParams.date || new Date();
+    $scope.todaysDate = $filter('date')(todaysDate, 'yyyy-MM-dd');
+
+    $scope.nextDate = (nextListing.data[0] ? nextListing.data[0].date : '');
+    $scope.prevDate = (prevListing.data[0] ? prevListing.data[0].date : '');
+    $scope.shows = thisDaysShows.data;
+    
+    console.log("Today: ", todaysDate);
+    console.log($scope.shows);
+    
+    $scope.setCurrShow = function(show){
+      $scope.currShow = show;
+      console.log(show);
+    };
   });
