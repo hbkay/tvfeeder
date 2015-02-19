96a64393ec65a59705c29fd3b7d0ec1cd9b1f42b
Belal Kohgadai <kohgadai@gmail.com>
2015-02-18 21:32:43 -0800

    added api retrieval into client-side routing, to be resolved with the home controller.

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -2,9 +2,39 @@
 
 angular.module('tvfeeder')
   .config(function($routeProvider) {
+
     $routeProvider
       .when('/', {
         templateUrl: '/app/home/home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl',
+        resolve: {
+          thisDaysShows:  thisDaysShows,
+          nextListing:    getNextlisting,
+          prevListing:    getPrevlisting
+        }
       });
   });
+
+
+// passes through resolve
+function thisDaysShows ($filter, $route, apiSvc) {
+
+  var date = $route.current.params.date || new Date();
+  var todaysDate = $filter('date')(date, 'yyyy-MM-dd');
+
+  return apiSvc.shows(todaysDate);
+}
+
+function getNextlisting ($filter, $route, apiSvc) {
+  var date = $route.current.params.date || new Date();
+  var todaysDate = $filter('date')(date, 'yyyy-MM-dd');
+
+  return apiSvc.nextListing(todaysDate);
+}
+
+function getPrevlisting ($filter, $route, apiSvc) {
+  var date = $route.current.params.date || new Date();
+  var todaysDate = $filter('date')(date, 'yyyy-MM-dd');
+
+  return apiSvc.prevListing(todaysDate);
+}
