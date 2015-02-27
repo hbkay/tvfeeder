26617f06d5c9f2f6378fc72927f8d26bb8231ff9
Belal Kohgadai <kohgadai@gmail.com>
2015-02-27 11:33:16 -0800

    fixed case issue in sql commands.

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -12,7 +12,7 @@ var path = require('path');
 var apiShowsByDate = function (req, res) {	
 	var date = req.params.date;
 
-	db.sequelize.query("SELECT * FROM Shows S, TVListings T WHERE S.id_listing=T.id AND T.date = :d",
+	db.sequelize.query("SELECT * FROM shows S, tvlistings T WHERE S.id_listing=T.id AND T.date = :d",
 		null, {raw: true}, {d: date})
 		.success(function(shows) {
 			res.json(shows);
@@ -22,7 +22,7 @@ var apiShowsByDate = function (req, res) {
 var apiPreviousListing = function (req, res) {	
 	var date = req.params.date;
 
-	db.sequelize.query("SELECT DATE_FORMAT(t.date,'%Y-%m-%d') as date FROM TVListings T WHERE t.date < :d ORDER BY DATE(t.date) DESC LIMIT 1;",
+	db.sequelize.query("SELECT DATE_FORMAT(T.date,'%Y-%m-%d') as date FROM tvlistings T WHERE T.date < :d ORDER BY DATE(T.date) DESC LIMIT 1;",
 		null, {raw: true}, {d: date})
 		.success(function(listingDate) {
 			res.json(listingDate);
@@ -32,7 +32,7 @@ var apiPreviousListing = function (req, res) {
 var apiNextListing = function (req, res) {	
 	var date = req.params.date;
 	
-	db.sequelize.query("SELECT DATE_FORMAT(t.date,'%Y-%m-%d') as date FROM TVListings T WHERE t.date > :d LIMIT 1;", 
+	db.sequelize.query("SELECT DATE_FORMAT(T.date,'%Y-%m-%d') as date FROM tvlistings T WHERE T.date > :d LIMIT 1;", 
 		null, {raw: true}, {d: date})
 		.success(function(listingDate) {
 			res.json(listingDate);
