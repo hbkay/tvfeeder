8f4c55ac036aa48fa2c8bbf53d242091c8bb4896
Belal Kohgadai <kohgadai@gmail.com>
2015-02-27 22:42:24 -0800

    route changes to latest previous date when current date is not in db.

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('tvfeeder')
-  .controller('HomeCtrl', function($scope, $routeParams, $filter, thisDaysShows, nextListing, prevListing) {
+  .controller('HomeCtrl', function($scope, $routeParams, $filter, $location, thisDaysShows, nextListing, prevListing) {
     var todaysDate = $routeParams.date || new Date();
     $scope.todaysDate = $filter('date')(todaysDate, 'yyyy-MM-dd');
 
     $scope.nextDate = (nextListing.data[0] ? nextListing.data[0].date : '');
     $scope.prevDate = (prevListing.data[0] ? prevListing.data[0].date : '');
     $scope.shows = thisDaysShows.data;
+
+    if (!$scope.shows.length) {
+      $location.url('/listing/' + $scope.prevDate);
+    }
     
     $scope.setCurrShow = function(show){
       if ($scope.currShow) {
